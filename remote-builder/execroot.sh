68fffcb4b175be6b741f6fd87ec5a749d79edefa
masaki-tamura <masaki-tamura@kccs.co.jp>
2020-09-03 13:38:33 +0900

    execroot sh

---
+++ remote-builder/execroot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+export PATH=$PATH:/usr/local/bin
+
+yum -y install python3 wget
+pip3 install ansible-bender selinux ansible
+wget https://download.opensuse.org/repositories/devel%3A/kubic%3A/libcontainers%3A/stable/CentOS_7/x86_64/conmon-2.0.20-1.el7.x86_64.rpm
+rpm -ivh conmon-2.0.20-1.el7.x86_64.rpm
+cd /etc/yum.repos.d/
+wget https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_7/devel:kubic:libcontainers:stable.repo
+yum -y install buildah podman
+sed -i -e "s/^mountopt = \"nodev,metacopy=on\"/mountopt = \"nodev\"/" /etc/containers/storage.conf
+systemctl start podman
+ansible-bender build main.yml
