83b3cae52ea5ba239258f351e5473576d87f01c8
masaki-tamura <masaki-tamura@kccs.co.jp>
2020-09-26 17:37:14 +0900

    fix err

--- execroot.sh
+++ execroot.sh
@@ -3,8 +3,8 @@ export PATH=$PATH:/usr/local/bin
 
 yum -y install python3 wget
 pip3 install ansible-bender selinux ansible
-wget https://download.opensuse.org/repositories/devel%3A/kubic%3A/libcontainers%3A/stable/CentOS_7/x86_64/conmon-2.0.20-1.el7.x86_64.rpm
-rpm -ivh conmon-2.0.20-1.el7.x86_64.rpm
+wget https://download.opensuse.org/repositories/devel%3A/kubic%3A/libcontainers%3A/stable/CentOS_7/x86_64/conmon-2.0.21-1.el7.x86_64.rpm
+rpm -ivh conmon-2.0.21-1.el7.x86_64.rpm
 cd /etc/yum.repos.d/
 wget https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_7/devel:kubic:libcontainers:stable.repo
 yum -y install buildah podman
