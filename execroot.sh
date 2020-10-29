1dff4561e6ce23241b3830a3646e6dd5e822a1c4
masaki-tamura <masaki-tamura@kccs.co.jp>
2020-10-29 15:34:19 +0900

    use dnf

--- execroot.sh
+++ execroot.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -xe
 export PATH=$PATH:/usr/local/bin
 
-yum -y install python3 wget git
+#yum -y install python3 wget git
+dnf -y install wget git
 pip3 install ansible-bender selinux ansible testinfra requests
-wget https://download.opensuse.org/repositories/devel%3A/kubic%3A/libcontainers%3A/stable/CentOS_7/x86_64/conmon-2.0.21-1.el7.x86_64.rpm
-rpm -ivh conmon-2.0.21-1.el7.x86_64.rpm
-cd /etc/yum.repos.d/
-wget https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_7/devel:kubic:libcontainers:stable.repo
-yum -y install buildah podman
+#wget https://download.opensuse.org/repositories/devel%3A/kubic%3A/libcontainers%3A/stable/CentOS_7/x86_64/conmon-2.0.21-1.el7.x86_64.rpm
+#rpm -ivh conmon-2.0.21-1.el7.x86_64.rpm
+#cd /etc/yum.repos.d/
+#wget https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_7/devel:kubic:libcontainers:stable.repo
+dnf -y install buildah podman
 sed -i -e "s/^mountopt = \"nodev,metacopy=on\"/mountopt = \"nodev\"/" /etc/containers/storage.conf
-systemctl start podman
+#systemctl start podman
 cd /home/cloud-user/workspace/ansible
 #ansible-bender build main.yml 
 ansible-bender --debug build main.yml 
